b2e620c07a0960cc6f70992cab518857472bf5b6
Rinat Tr <rinattregerman@pursuit.org>
2018-11-18 19:04:26 -0500

    done not styled

--- index.js
+++ index.js
@@ -14,20 +14,35 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
   let selectMenu = document.querySelector("#movies");
+  let infoBox = document.querySelector(".movieInfo");
   let moviesArr = [];
-  let fetchObj = fetch("https://ghibliapi.herokuapp.com/films/")
+
+  const displayInfo = (id) => {
+    infoBox.innerHTML =
+      `<p>Title: ${moviesArr[id].title}</p>
+       <p>Director: ${moviesArr[id].director}</p>
+       <p>Description: ${moviesArr[id].description}</p>
+       <p>Release date: ${moviesArr[id].release_date}</p>
+       <p>Rotten Tomato Score: ${moviesArr[id].rt_score}</p>
+       `
+  }
+
+  fetch("https://ghibliapi.herokuapp.com/films/")
               .then(response => {
                 return response.json();
               }).then(movies => {
                 moviesArr = movies;
-                moviesArr.forEach(movie,i => {
+                console.log(moviesArr);
+                moviesArr.forEach((movie,i) => {
                   let opt = document.createElement('option');
                   opt.innerText = movie.title;
+                  opt.setAttribute("id",i);
                   selectMenu.appendChild(opt);
                 })
-              }).then(()=> {
+              });
 
+  selectMenu.addEventListener("change", (event) => {
+                let idx = parseInt(event.target.selectedOptions[0].id); //gets the id which is the idx
+                displayInfo(idx);
               })
-
-
 });
